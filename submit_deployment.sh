415da98b9260b85423741e4d38cf247204a82c30
Alexander Nygård <alexander.k.nygard@gmail.com>
2024-06-07 15:22:31 +0200

    fix typo, add round numbers

--- submit_deployment.sh
+++ submit_deployment.sh
@@ -2,16 +2,4 @@
 
 
 # Base deployment
-sbatch submit_job.sh --option="deployment"
-
-# Blind simulation
-sbatch submit_job.sh --option="simulation"
-
-# Batch size simulations
-sbatch submit_job.sh --option="simulation" --batch_size="non-iid"
-sbatch submit_job.sh --option="simulation" --local_epochs="non-iid"
-sbatch submit_job.sh --option="simulation" --data_volume="non-iid"
-sbatch submit_job.sh --option="simulation" --data_labels="non-iid"
-
-# Realistic simulations
-sbatch submit_job.sh --option="simulation" --batch_size="non-iid" --local_epochs="non-iid" --data_volume="non-iid" --data_labels="non-iid"
+sbatch submit_job.sh --option="deployment" --num_clients=100 --batch_size="noniid" --local_epochs="noniid" --data_volume="noniid" --data_labels="noniid"
--- submit_simulations.sh
+++ submit_simulations.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-# Deployment
-sbatch submit_job.sh --option="deployment"
+# Blind simulation
+sbatch submit_job.sh --option="simulation" --num_clients=100
+
+# Batch size simulations
+sbatch submit_job.sh --option="simulation" --num_clients=100 --batch_size="noniid"
+sbatch submit_job.sh --option="simulation" --num_clients=100 --local_epochs="noniid"
+sbatch submit_job.sh --option="simulation" --num_clients=100 --data_volume="noniid"
+sbatch submit_job.sh --option="simulation" --num_clients=100 --data_labels="noniid"
+
+# Realistic simulations
+sbatch submit_job.sh --option="simulation" --num_clients=100 --batch_size="noniid" --local_epochs="noniid" --data_volume="noniid" --data_labels="noniid"
